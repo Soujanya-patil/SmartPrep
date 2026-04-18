fd1e1d0a726bdc39a26005e22716a449b637d1a0
Soujanya <soujanya.patil2003@gmail.com>
2026-04-18 21:51:29 +0530

    Phase 2 complete - Spring Boot project setup

---
+++ src/test/java/com/smartprep/SmartprepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartprep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartprepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
